1b01ad5c51f1918a7973798c92b5a130772df8eb
seikirowins-bie <rowins.bie.rb@gmail.com>
2021-03-21 18:19:05 +0800

    implemented navigation guard of vue

--- resources/js/routing/router.js
+++ resources/js/routing/router.js
@@ -3,6 +3,13 @@ import CreditorPage from './../components/creditor/index'
 import InventoryPage from './../components/inventory/index'
 import LoginPage from './../components/login/index'
 //end of components
+
+//state management
+import Auth from '../StateManagement/Authentication';
+
+// end of state management
+
+
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
@@ -12,19 +19,44 @@ const router = new VueRouter({
         {
             path:'/',
             name:'login-page',
-            component:LoginPage
+            component:LoginPage,
+           
         },
         {
             path:'/creditors',
             name:'creditor-page',
-            component:CreditorPage
+            component:CreditorPage,
+            meta:{
+                requiresAuth:true
+            }
         },
         {
             path:'/inventory',
             name:'inventory-page',
-            component:InventoryPage
+            component:InventoryPage,
+            meta:{
+                requiresAuth:true
+            }
         }
     ]
 });
+
+
+router.beforeEach((to,from,next)=>{
+    if(to.matched.some(record => record.meta.requiresAuth))
+    {
+        if(!Auth.getters.isAuthenticated)
+        {
+            next({
+                name:'login-page'
+            });
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
+
 
 export default router;
