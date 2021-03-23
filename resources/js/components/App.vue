d9b875a7082caf945ead9b644781568c85a677af
seikirowins-bie <rowins.bie.rb@gmail.com>
2021-03-23 21:47:30 +0800

    developed the inital design of navigation bar

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,23 +1,30 @@
 <template>
-   <div v-bind:class="isAuth ? 'container-fluid' :  'container'" class="mt-5">
-       <div class="row">
-            <router-view ></router-view>
-       </div>
-       
-   </div>
+  <div v-bind:class="isAuth ? 'container-fluid' : 'container'" class="mt-5">
+    <div v-if="isAuth" class="row">
+      <div class="col-lg-12">
+        <navigationBar />
+      </div>
+    </div>
+    <div class="row">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <script>
-import Auth from '../StateManagement/Authentication';
-    export default {
-        mounted() {
-            
-        },
-        computed:{
-            isAuth()
-            {
-                return Auth.getters.isAuthenticated;
-            }
-        }
-    }
+// import the layout when authenticated
+import navigationBar from "./layout/navigationBar/navigation-bar";
+
+import Auth from "../StateManagement/Authentication";
+export default {
+  components: {
+    navigationBar,
+  },
+  mounted() {},
+  computed: {
+    isAuth() {
+      return Auth.getters.isAuthenticated;
+    },
+  },
+};
 </script>
