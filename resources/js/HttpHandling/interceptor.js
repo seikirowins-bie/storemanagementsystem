c69642ace335fbc93a1e82f0f4d1b61374e4e784
seikirowins-bie <rowins.bie.rb@gmail.com>
2021-03-27 15:40:52 +0800

    to do: fixed bug in interceptor 401 redirection

--- resources/js/HttpHandling/interceptor.js
+++ resources/js/HttpHandling/interceptor.js
@@ -1,3 +1,4 @@
+import Auth from '../StateManagement/Authentication';
 export default function InterceptHTTPRequest() {
     axios.interceptors.response.use(undefined, function(err) {
         const error = err.response;
@@ -6,7 +7,14 @@ export default function InterceptHTTPRequest() {
             error.config &&
             !error.config.isRetryRequest
         ) {
+            Auth.dispatch('signOut');
+            
             console.log("hahaha");
+        }
+
+        if(error.status == 429)
+        {
+            console.log("Too many request");
         }
 
         return Promise.reject(err);
